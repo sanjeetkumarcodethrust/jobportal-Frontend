ce65cef6acca3741dcc4500afa38c7d1d139676d
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-05-05 16:52:07 +0530

    Update API base URL configuration and add proxy settings in Vite config

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,12 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   }
 })
